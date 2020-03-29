d99c0117da0471beb7661a95993ac0f3773ea470
CG-DTE <anveshsharma99@email.com>
2020-03-29 18:39:24 +0530

    SM_1002 code changes

--- src/MyFirstApplication.java
+++ src/MyFirstApplication.java
@@ -3,11 +3,11 @@ public class MyFirstApplication {
 	
 	public static void main(String[]args) {
 		System.out.println("Hello world1");
-		System.out.println("Hello world2");
+		System.out.println("Hello world2:: EDIt 1002");
 		for(int i=0;i<3;i++)
 			System.out.println("HELLO KK");
 
-		System.out.println("Hello world3");
+		System.out.println("Hello world3:: EDIt 1002");
 
 	}
 
