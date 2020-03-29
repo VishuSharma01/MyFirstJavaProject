bef8c7957574ebb380f6fd85fd5c58975822be3f
Trinity <anveshsharma99@email.com>
2020-03-29 16:36:26 +0530

    first commit

---
+++ src/MyFirstApplication.java
@@ -0,0 +1,11 @@
+
+public class MyFirstApplication {
+	
+	public static void main(String[]args) {
+		System.out.println("Hello world1");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
+
+	}
+
+}
