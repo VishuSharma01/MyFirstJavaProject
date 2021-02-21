ee0b0b6087462ee8975bee72ee5b9966728c7b64
VishuSharma01 <sharmaanvesh99@gmail.com>
2021-02-21 20:58:27 +0530

    abc

--- src/MyFirstApplication.java
+++ src/MyFirstApplication.java
@@ -4,11 +4,5 @@ public class MyFirstApplication {
 	public static void main(String[]args) {
 		System.out.println("Hello world1");
 		System.out.println("Hello world2:: EDIT 1003");
-		for(int i=0;i<3;i++)
+		for (int i = 0; i < 3; i++)
 			System.out.println("HELLO KK");
-
-		System.out.println("Hello world3:: EDIT 1003");
-
-	}
-
-}
